d9fdaf20cd646946f7435a5d02b2b10c138c7d9b
MaxSpiro <maxspiridonov04@gmail.com>
2022-10-15 15:09:27 -0700

    use static images

---
+++ src/projects.ts
@@ -0,0 +1,57 @@
+export const personal = [
+  {
+    className: 'max-w-[600px]',
+    width: 800,
+    imgName: 'scribefps.png',
+    url: 'https://scribefps.netlify.app',
+    buttonText: 'Open on Netlify',
+    title: 'The Scribe official website',
+    description:
+      'Includes a user-friendly admin page and content management system so that the new issues ,may be uploaded by the editors.',
+  },
+  {
+    className: 'max-w-[500px]',
+    width: 800,
+    imgName: 'twitter-bot.png',
+    url: 'https://twitter.com/gothmoneybot',
+    buttonText: 'Open on Twitter',
+    title: 'Goth Money & Sad Boys Lyric Bot',
+    description:
+      'A twitter bot I scheduled to post lyrics from my favorite artists every 6 hours',
+  },
+  {
+    className: 'max-w-[1000px]',
+    width: 1000,
+    imgName: 'my-google-wallet.png',
+    url: '',
+    buttonText: 'Not yet live',
+    title:
+      "My Google Wallet, a web app that uses the Torus network to create private keys from users' Google accounts.",
+    description:
+      'With the private key, my app providers users with addresses and wallets for major blockchains like Ethereum, Cosmos Hub, Bitcoin, Polygon, and more.',
+  },
+  {
+    className: 'max-w-[1000px]',
+    width: 1000,
+    imgName: 'solana.png',
+    buttonText: 'Open on Vercel',
+    title:
+      'Solana Socials, a social app that allows users to connect with Solana web3 wallets as well as theGoogle wallet reated by the Torus network.',
+    description:
+      'This app uses database storage to store user data, but I hope to migrate to a blockchain solution in the future.',
+    url: 'https://solana-social.vercel.app/',
+  },
+]
+
+export const work = [
+  {
+    width: 1400,
+    imgName: 'defispot.png',
+    url: 'https://app.defispot.com',
+    buttonText: 'Open on app.defispot.com',
+    title:
+      'A decentralized, multichain exchange built on the THORChain protocol.',
+    description:
+      'I implemented the social login flow and key generation seen in My Google Wallet, worked onthe UI/UX of the swap interface, improving asset selection screens and increasing transparency with network/protocol fees, and worked on the mobile app.',
+  },
+]
